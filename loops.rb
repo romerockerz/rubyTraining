a2f30d564f2bf9fa940e9ef72842c35e1ee62924
romerockerz <70739928+romerockerz@users.noreply.github.com>
2020-09-08 15:45:42 +0530

    Update loops.rb

--- loops.rb
+++ loops.rb
@@ -1,36 +1,4 @@
-Skip to content
-Search or jump to…
 
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@romerockerz 
-Learn Git and GitHub without any code!
-Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
-
-
-rajeshpillai
-/
-dassault-ruby-batch-3
-1
-01
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-dassault-ruby-batch-3/2-loops.rb /
-@rajeshpillai
-rajeshpillai Conditionals and Loops
-Latest commit b2d6044 13 minutes ago
- History
- 1 contributor
-76 lines (58 sloc)  866 Bytes
   
 # Simple Loop
 # loop do 
@@ -107,15 +75,4 @@ for i in nums do
   puts i
 end
 
-© 2020 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+
